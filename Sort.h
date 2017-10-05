e61abbc9b10e4ab44bc3c04a34ebb714924ba84a
ltb427 <1123039786@qq.com>
2017-10-05 13:50:44 +0800

    格式说明

---
+++ Sort.h
@@ -0,0 +1,32 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	//√∞≈›≈≈–Ú
+	void BubbleSort(int arr[], const size_t len);
+	//≤Â»Î≈≈–Ú
+	void InsertionSort(int arr[], const size_t len);
+	//œ£∂˚≈≈–Ú
+	void ShellSort(int arr[], const size_t len);
+	//—°‘Ò≈≈–Ú
+	void SelectSort(int arr[], const size_t len);
+	//øÏÀŸ≈≈–Ú
+	void QuickSort(int arr[], const size_t len);
+	//πÈ≤¢≈≈–Ú
+	void MergeSort(int arr[], const size_t len);
+	//∂—≈≈–Ú
+	void HeapSort(int arr[], const size_t len);
+	//º∆ ˝≈≈–Ú
+	void CountSort(int arr[], const size_t len);
+	//Õ∞≈≈–Ú
+	void BucketSort(int arr[], const size_t len);
+	//ª˘ ˝≈≈–Ú
+	void RadixSort(int arr[], const size_t len);
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
